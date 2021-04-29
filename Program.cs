f4b14a3f5273a80f8b9d88d62d50eee6e0eaa542
dikoleshubh <dikole.shubh@gmail.com>
2021-04-29 23:04:33 +0530

    Happy/Sad Mood check

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mood_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
