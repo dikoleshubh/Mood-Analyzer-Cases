609dcf77e18e7c3e22730656ff129dfdbb003d58
dikoleshubh <dikole.shubh@gmail.com>
2021-04-30 13:13:11 +0530

    Happy/Sad Mood check-EXCEPTION HANDLING

---
+++ Exception.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mood_Test
+{
+    public class MoodAnalyzerException : Exception
+    {
+        ExceptionType type;  //Create instance of custom exception type
+        public string message;
+
+        public enum ExceptionType  //enum for define constants
+        {
+            NULL_EXCEPTION, EMPTY_EXCEPTION
+        }
+        public MoodAnalyzerException(ExceptionType type, string message) : base(message)  //parameterized constructor and also using bsae method
+        {
+            this.type = type;
+        }
+    }
+}
